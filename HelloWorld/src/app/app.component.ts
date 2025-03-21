d9872c83ff81a94cfc4b9643b29960caca1384bb
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2025-03-22 00:32:38 +0530

    [Vansh Verma] Add: Completed UC1 Task

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  title:string = 'Hello From BridgeLabz';
 }
