5da315b6f7cf343e9497290c9cb0b5911f52b308
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2025-03-22 12:11:31 +0530

    [Vansh Verma] Add: UC5 Task Completed

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -14,9 +14,21 @@ export class AppComponent {
   title:string = 'Hello From BridgeLabz';
   imgUrl: string = 'bridgeLabz.jpeg';
   userName:string='';
+  nameError:string='';
 
   get welcomeMessage():string{
     return this.userName?`Hello ${this.userName} from BridgeLabz, `:'';
+  }
+
+  onInput(event: Event) {
+    console.log('Input Event Occurred!', event);
+    const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+
+    if (nameRegex.test(this.userName)) {
+      this.nameError = '';
+    } else {
+      this.nameError = 'Name is Incorrect!';
+    }
   }
   openBridgeLabz() {
     window.open('https://www.bridgelabz.com', '_blank');
