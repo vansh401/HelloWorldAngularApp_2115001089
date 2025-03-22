d7973e322ade82f89dfe60449a5192682887f45a
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2025-03-22 11:50:39 +0530

    [Vansh Verma] Add: UC4 Task Completed

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
+  standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title:string = 'Hello From BridgeLabz';
   imgUrl: string = 'bridgeLabz.jpeg';
+  userName:string='';
+
+  get welcomeMessage():string{
+    return this.userName?`Hello ${this.userName} from BridgeLabz, `:'';
+  }
   openBridgeLabz() {
     window.open('https://www.bridgelabz.com', '_blank');
   }
